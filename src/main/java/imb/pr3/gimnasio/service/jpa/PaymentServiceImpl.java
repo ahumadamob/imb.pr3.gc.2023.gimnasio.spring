2651dc0b33a6c66b38ec4d93f90eb6a8d009bafb
l98c <leanmarc31@gmail.com>
2023-09-29 12:44:37 -0300

    Correccion en la implementacion

--- src/main/java/imb/pr3/gimnasio/service/jpa/PaymentServiceImpl.java
+++ src/main/java/imb/pr3/gimnasio/service/jpa/PaymentServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import imb.pr3.gimnasio.entity.Payment;
 import imb.pr3.gimnasio.repository.PaymentRepository;
 import imb.pr3.gimnasio.service.IPaymentService;
 
+@Service
 public class PaymentServiceImpl implements IPaymentService{
 	
 	@Autowired
