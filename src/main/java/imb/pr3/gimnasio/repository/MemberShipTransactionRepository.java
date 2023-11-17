b1378243f491750391a7ea7f64f1cd8db2875501
FerCod <fer__co@hotmail.com>
2023-11-17 20:17:05 -0300

    End Points membershipTransaction

--- src/main/java/imb/pr3/gimnasio/repository/MemberShipTransactionRepository.java
+++ src/main/java/imb/pr3/gimnasio/repository/MemberShipTransactionRepository.java
@@ -1,9 +1,12 @@
 package imb.pr3.gimnasio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import imb.pr3.gimnasio.entity.MemberShipTransaction;
 
 public interface MemberShipTransactionRepository extends JpaRepository<MemberShipTransaction, Integer> {
 
+	List<MemberShipTransaction> findByEnable(boolean enable);
 }
